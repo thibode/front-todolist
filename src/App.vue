9bebe88d71b53364f7341b0c59e089b5d499cee8
thibode <thibode@MBP-de-Thibault.lan>
2023-05-09 17:03:35 +0200

    crud ajouté pour la todolist

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,22 @@
 <script setup>
-import AccueilApp from './components/AccueilApp.vue'
+import Home from './components/Home.vue'
+import TodoList from './components/ToDoList.vue'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <AccueilApp msg="Réaliser une to do list avec VueJs!" />
-    </div>
+    <Home/>
   </header>
-
   <main>
-
+    <div class="todo-placement">
+      <TodoList />
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-.wrapper {
-  margin: 0 auto;
-}
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+body {
+  background-color: aliceblue;
 }
 </style>
